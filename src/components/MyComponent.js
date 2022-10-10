db73bd2312a9282fad6e72f8847249902280cc9e
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2022-10-10 13:41:47 +0700

    learn Components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class components
+// function components
+import React from "react";
+
+class MyComponents extends React.Component {
+    //jsx
+    render() {
+        return (
+            <div>
+                My first Component....
+                {Math.random()};
+            </div>
+        );
+    }
+}
+
+export default MyComponents;
