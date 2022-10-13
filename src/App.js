7eb2263b67bef7c6833b03203abaccf69b31f574
Pham Thai Nguyen <phamthainguyen0703@gmail.com>
2022-10-13 15:51:38 +0700

    setState

--- src/App.js
+++ src/App.js
@@ -3,31 +3,47 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponents from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div>
-      <p>
-        It's time
-      </p>
-      <MyComponents>
 
-      </MyComponents>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       It's time
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          It's time
+        </p>
+        <MyComponents>
+
+        </MyComponents>
+      </div>
+    );
+  }
 }
+
+
+
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           It's time
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,28 @@ import React from "react";
 
 class MyComponents extends React.Component {
     //jsx
+
+    state = {
+        name: `Quyên`,
+        addr: `home`,
+        age: `20`
+    };
+
+    handleClick = (event) => {
+        console.log(this.state);
+
+        this.setState({
+            name: `Nguyên`,
+            age: Math.floor((Math.random() * 100) + 1),
+        })
+    }
+
     render() {
         return (
             <div>
-                My first Component....
-                {Math.random()};
+                My name is: {this.state.name}, im {this.state.age} years old
+
+                <button onClick={(event) => { this.handleClick(event) }}>Click</button>
             </div>
         );
     }
